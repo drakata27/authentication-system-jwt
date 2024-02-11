08e377fdfa99be70024b20da62e94650b7f3653e
Aleksandar Drakaliyski <theall3xander@gmail.com>
2024-02-11 18:06:39 +0000

    Created serializers for the user

---
+++ backend/api/urls.py
@@ -0,0 +1,7 @@
+from rest_framework_simplejwt.views import TokenRefreshView
+from django.urls import path
+
+urlpatterns = [
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path()
+]
